6b49fa4247192e3a9bb13ff1deeac3b77df277da
Coody <coody0829@gmail.com>
2017-03-02 09:47:39 +0800

    [ fix , refactor ] 修正 storyboard 、修正 ViewController 的 Block 排版

--- TestColckManager/TestColckManager/ViewController.m
+++ TestColckManager/TestColckManager/ViewController.m
@@ -27,22 +27,23 @@ NSUInteger const K_REGISTER_PHONE_CLOCK = 11111;
 
 - (IBAction)pressedButton:(UIButton *)sender {
     __weak __typeof(self) weakSelf = self;
-    _clock = [[ClockManager sharedInstance] getRandomClockWithSecond:10 
-                                                   withResponseBlock:^(NSUInteger second) {
-                                                       // 使用 weakSelf 避免記憶體遺漏
-                                                       __strong __typeof(weakSelf) strongSelf = weakSelf;
-                                                       // 如果  ViewController 解構就不處理
-                                                       if ( strongSelf != nil ) {
-                                                           NSString *buttonTitle;
-                                                           if ( second == 0 ) {
-                                                               buttonTitle = @"開始";
-                                                           }
-                                                           else{
-                                                               buttonTitle = [NSString stringWithFormat:@"倒數 %2lu 秒！" , (unsigned long)second];
-                                                           }
-                                                           [strongSelf->_testButton setTitle:buttonTitle forState:UIControlStateNormal];
-                                                       }
-                                                   }];
+    _clock = [[ClockManager sharedInstance] getRandomClockWithSecond:10 withResponseBlock:^(NSUInteger second) {
+        
+        // 使用 weakSelf 避免記憶體遺漏
+        __strong __typeof(weakSelf) strongSelf = weakSelf;
+        // 如果  ViewController 解構就不處理
+        if ( strongSelf != nil ) {
+            NSString *buttonTitle;
+            if ( second == 0 ) {
+                buttonTitle = @"開始";
+            }
+            else{
+                buttonTitle = [NSString stringWithFormat:@"倒數 %2lu 秒！" , (unsigned long)second];
+            }
+            [strongSelf->_testButton setTitle:buttonTitle forState:UIControlStateNormal];
+        }
+        
+    }];
     
 //    
 //    [[ClockManager sharedInstance] getClockWithSecond:60 
